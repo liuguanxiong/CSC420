929e50e9b2f85a9838cd614d795693da9d1c4803
ANALOG\gliu3 <Guanxiong.Liu@analog.com>
2018-10-10 21:55:24 -0400

    update

--- a2/assignment2.m
+++ a2/assignment2.m
@@ -1,87 +1,43 @@
-close all;
-%% open the image for reading
-img = imread('synthetic.png');
-img = double(img);
-img = mean(img,3);
 
-%% perform base-level smoothing to supress noise
-imgS = img;
-clear responseDoG responseLoG
-k = 1.1;
-sigma = 2.0;
-s = k.^(1:30)*sigma;
-responseLoG = zeros(size(img,1),size(img,2),length(s));
+    %% 1. read images
+    origin1 = imread('book.jpeg');
+    origin2 = imread('findbook.png');
+    img1=single(rgb2gray(origin1));
+    img2=single(rgb2gray(origin2));
 
-%% Filter over a set of scales
-for si = 1:length(s)
-    sL = s(si);
-    filter = 2*ceil(3*sL)+1;
-%     filter = ceil(sL);
-    HL = fspecial('log',filter,sL);
-    imgFiltL = conv2(imgS,HL,'same');
-    %Compute the LoG
-    responseLoG(:,:,si)  = (sL^2)*imgFiltL;
-end
+    %% 2. compute sift
+    [f1,d1]=vl_sift(img1);
+    [f2,d2]=vl_sift(img2);
+    
+    %% 3. compute the distance
+    d1 = double(transpose(d1));
+    d2 = double(transpose(d2));
+    dist = pdist2(d1,d2,'euclidean'); % dist = 336 x 14625
+    
+    %% 4. get matched points that passed the threshold
+    num = size(f1,2); % number of filter interest points
+    from = zeros(num,2);
+    to = zeros(num,2);
+    T = 0.3;
 
-%% detect and draw interest points
-fg = figure;imagesc(img);axis image;hold on;colormap gray;
-drawnow;
-
-
-T = 20;
-
-figure(fg);
-for co=3:size(img,2)-2
-    fprintf('(row) = (%i) \n',co);
-    for row=3:size(img,1)-2
-        %% find peak
-        f = squeeze(responseLoG(row,co,:));
-        [fMax,fmaxLocs] = findpeaks(f);         %maxima
-        [fMin,fminLocs] = findpeaks(-f);        %minima
-        
-        
-        %% maxima
-        for i = 1:numel(fmaxLocs)
-            % threshold test
-            if fMax(i) < T
-                continue;
-            end
-            
-            % check local extrama
-            sc = s(fmaxLocs(i));
-            [nx,ny,nz] = meshgrid(co-2:co+2,row-2:row+2,fmaxLocs(i));
-            inds = sub2ind(size(responseLoG),ny,nx,nz);
-            df = responseLoG(inds(13))-responseLoG(inds);
-            df(5)=[];               %don't compare to itself
-            if(min(df)>0)
-                % plot circle
-                xc = sc*sin(0:0.1:2*pi)+co;
-                yc = sc*cos(0:0.1:2*pi)+row;
-                plot(xc,yc,'r');
-            end
+    for i=1:num % number of filter interest points
+        idx = find(dist(i,:) == min(dist(i,:)));
+        numerator = dist(i,idx(1));
+        dist(i,idx) = max(dist(i,:)); % change the minimum to maximum
+        denominator = min(dist(i,:));
+        if numerator/denominator <= T
+            from(i,:) = [f1(1,i) f1(2,i)];
+            to(i,:) = [f2(1,idx) f2(2,idx)];
         end
-        
-        
-        %% minima
-        for i = 1:numel(fminLocs)
-            % threshold test
-            if fMin(i) < T
-                continue;
-            end
-            
-            % check local extrama
-            sc = s(fminLocs(i));
-            [nx,ny,nz] = meshgrid(co-1:co+1,row-1:row+1,fminLocs(i));
-            inds = sub2ind(size(responseLoG),ny,nx,nz);
-            df = responseLoG(inds(5))-responseLoG(inds);
-            df(5)=[];               %don't compare to itself
-            if(max(df) <0)
-                % plot circle
-                xc = sc*sin(0:0.1:2*pi)+co;
-                yc = sc*cos(0:0.1:2*pi)+row;
-                plot(xc,yc,'g');
-            end
-        end
-        
     end
-end
+    
+    %% 5. remove the points that failed the threshold test
+    from( all(~from,2), : ) = [];
+    to( all(~to,2), : ) = [];
+    
+    %% 6. plot the matched points
+    figure; ax = axes;
+    showMatchedFeatures(origin1,origin2,from,to,'montage','Parent',ax);
+    title(ax, 'Candidate point matches');
+    legend(ax, 'Matched points 1','Matched points 2');
+
