e5668e1a63e06418e7c8e6a8a61a46194937ab45
ANALOG\gliu3 <Guanxiong.Liu@analog.com>
2018-11-17 18:28:51 -0500

    finish 1

--- a5/facenet.py
+++ a5/facenet.py
@@ -4,7 +4,9 @@ from keras.models import model_from_json
 import cv2
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+import pandas as pd
 
 
 def load_facenet():
@@ -53,7 +55,9 @@ def load_dataset():
 def k_clustering(model):
     imgs_des = []
     for f in os.listdir('./saved_faces'):
-        imgs_des.append(img_to_encoding(cv2.imread('./saved_faces/'+f),model)[0])
+        embedding = img_to_encoding(cv2.imread('./saved_faces/'+f),model)[0]
+        np.save('./q1_data/embedding/'+f[:-4]+'.npy',embedding)
+        imgs_des.append(embedding)
     imgs_des = np.array(imgs_des)
     kmeans = KMeans(n_clusters=6).fit(imgs_des)
     return kmeans
@@ -70,11 +74,33 @@ def inverted_index(kmeans, model):
         else:
             dic[classification[0]] = [f]
     return dic
-    
+
+def find_match(kmeans,model,dic):
+    df = pd.DataFrame(columns=['input_img','matching_imgs'])
+    for f in os.listdir('./input_faces'):
+        img = cv2.imread('./input_faces/'+f)
+        img = cv2.resize(img,(96,96))
+        embedding = img_to_encoding(img,model)
+        classification = kmeans.predict(embedding)
+        if np.linalg.norm(kmeans.cluster_centers_[classification[0]] - embedding[0]) < 0.5:
+            df.loc[-1] = [f,dic[classification[0]]]
+            df.index += 1
+            df = df.sort_index()
+        else:
+            df.loc[-1] = [f,'no matches']
+            df.index += 1
+            df = df.sort_index()
+    df.to_csv('./q2_data/matching.csv')
+
 if __name__ == "__main__":
     load_dataset()
     fr_model = load_facenet()
+
+    # 1c 1e save embeddings and k-means clustering
     kmeans = k_clustering(fr_model)
+
+    # 1f save visual word and build inverted_index
     save_cluster_center(kmeans)
     dic = inverted_index(kmeans,fr_model)
-    print(dic)
+
+    find_match(kmeans,fr_model,dic)
