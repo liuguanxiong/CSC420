2e428909f990c5d522fca404cb7574b12f3229a2
ANALOG\gliu3 <Guanxiong.Liu@analog.com>
2018-11-16 22:20:48 -0500

    update

--- a5/assignment5.py
+++ a5/assignment5.py
@@ -5,21 +5,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import numpy as np
 
-if __name__ == "__main__":
-    train = datasets.FashionMNIST('./data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ]))
-    test = datasets.FashionMNIST('./data', train=False, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ]))
-                
-    
-    classes = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+def bar_graph(train, test, classes):
     counter = {}
     for i in range(len(train)):
         image, label = train[i]
@@ -35,4 +25,26 @@ if __name__ == "__main__":
         else:
             counter[classes[label.item()]] += 1
     
-    print(counter)
+    num_of_instances = []
+    for c in classes:
+        num_of_instances.append(counter[c])
+    y_pos = np.arange(len(classes))
+    plt.bar(y_pos,num_of_instances,width=0.4,align='center',alpha=0.5)
+    plt.xticks(y_pos,classes)
+    plt.title('number of instances of each class')
+    # plt.show()
+
+if __name__ == "__main__":
+    train = datasets.FashionMNIST('./data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ]))
+    test = datasets.FashionMNIST('./data', train=False, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ]))
+    classes = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    
+    bar_graph(train, test, classes)
